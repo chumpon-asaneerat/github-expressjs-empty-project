2f359d09a6e65015bccbfb7ec773c93dceae1d36
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-30 19:07:50 +0700

    server.js update tabulator path

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const dist_libs = [
     /* jquery-org-chart */
     { "route": "/dist", "path": "jquery-org-chart-2.1.3" },
     /* tabulator */
-    { "route": "/dist", "path": "tabulator-4.3.0" },
+    { "route": "/dist", "path": "tabulator-4.4.1" },
     /* ace */
     { "route": "/dist/js", "path": "ace-1.4.5" },
     /* simplebar */
